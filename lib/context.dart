9f90c74ec839db947aaac9f3f38e608fd6eb424a
yenole <xusir92@gmail.com>
2020-04-21 17:53:17 +0800

    feat: 适配easy_loading

---
+++ lib/context.dart
@@ -0,0 +1,16 @@
+part of easy_tips;
+
+
+class EasyWrapContext extends StatelessWidget {
+  final Widget child;
+  static BuildContext context;
+
+  EasyWrapContext({Key key, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    EasyWrapContext.context = context;
+    debugPrint('>>>>>>$context');
+    return child;
+  }
+}
---
+++ lib/easy_tips.dart
@@ -0,0 +1,7 @@
+library easy_tips;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+part 'context.dart';
+part 'easy_loading.dart';
